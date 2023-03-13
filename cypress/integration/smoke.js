7be0f70ed21c890d09f22f0b68b75ae1dfd8fbf2
eduardo aleixo <eduardo@pyroscope.io>
2023-03-13 15:44:07 +0000

    test autocomplete

--- cypress/integration/smoke.js
+++ cypress/integration/smoke.js
@@ -14,4 +14,20 @@ describe('smoke', () => {
 
     cy.wait('@renderRequest');
   });
+
+  it('auto completes', () => {
+    cy.visit(
+      'http://localhost:3000/d/single-panel/single-panel-dashboard?orgId=1&editPanel=2&from=1678718500926&to=1678719100926'
+    );
+
+    cy.intercept('**/api/datasources/proxy/1/render/api/apps', {
+      fixture: 'appNames',
+    }).as('appNames');
+
+    // CTRL + SPACE
+    cy.get('.query-editor-row [data-slate-editor]').clear().type(`{ctrl}`).trigger('keydown', { keyCode: 32 });
+
+    // 10 elements + header
+    cy.get('ul.typeahead li').should('have.length', 11);
+  });
 });
